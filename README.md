1b4ef420d713c2c2a26b264d025fac8abde0e013
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2020-09-17 15:27:32 +0100

    add: countdown timer, function for updating buttons, readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Whack-a-mole
+Whack-a-mole game implemented in ES5 for a technical interview. It took me TBD hours over 3 days. 
--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 //todo
-//fix spam clicking start game
+//add more moles and allow them to flow onto new lines
+//mobile/IE testing
+
+//fix: timer jumps over the (max - 1)th second
 //visible for random amount of time
-//time limit
 //fix jumping when moving down
-//fix start position and overflow
-//IE/responsiveness implementation/testing 
-    //(make moles flow onto new lines if insufficient space; stack buttons)
 //accessibility (button labels, img labels/alts)
     //keydown for buttons (with labels to say what key to press)
     //keydown for moles
-//docs
 
 //nice-to-have
 //prettier text/game (rounder mole holes)
-//unit test
 //sound
 //cursor animation/change on successful hit
 
@@ -22,20 +19,26 @@ const popupTimer = 1000; //todo convert to rand
 /** Distance to move moles up and down by in rem */
 const popupDistance = 2; //todo but game area is in px
 /** Total game length in milliseconds */
-const totalGameTime = 60000;
+const totalGameTime = 20000;
+/** Time between one mole popping up and the next */
+const timeBetweenMoles = 2500;
 /** Interval id of current game. Used to pause the game */
 let game;
 /** HTML element of the game area */
 let gamearea;
 /** Game score */
 let score = 0;
-/** Game countdown timer */
-let gameTimer = totalGameTime; 
+/** Timer ID for game countdown timer */
+let gameTimer;
+/** Time to start counting down from the next time the timer is started or restarted */
+let timeToCountdownFrom = totalGameTime;
 /** References to the html element and timer ID for the current mole. 
  * Used to popdown the mole if it is hit */
 let mole, timer;
 /** Whether the game is currently paused */
 let paused = true;
+/** Whether the current game is over */
+let ended = false;
 
 window.onload = (e) => {
     //set up buttons
@@ -49,9 +52,17 @@ window.onload = (e) => {
     //set up click listener
     gamearea = document.getElementById("game");
     gamearea.addEventListener('click', clickHandler);
+    
+    //set timer's initial value
+    setTimerValue(document.getElementById("timeLeft"), timeToCountdownFrom);
 }
 
+/**
+ * Start the game if it was previously paused
+ */
 function startGame() {
+    //prevent unpausing game if game is over
+    if(ended) return;
     //prevent multiple 'games' from being started
     if(!paused) return;
     paused = false;
@@ -60,48 +71,124 @@ function startGame() {
     updateGameStateText(false, "Game in progress.");
 
     //toggle buttons
-    document.getElementById("pauseBtn").classList.remove("disabled");
-    document.getElementById("pauseBtn").setAttribute("aria-pressed", false);
-    document.getElementById("startBtn").classList.add("disabled");
-    document.getElementById("startBtn").setAttribute("aria-pressed", true);
+    updateButton("pauseBtn", true);
+    updateButton("startBtn", false);
 
     let moles = document.getElementsByClassName("mole");
 
+    //do first mole
+    mole = selectMole(moles);
+    timer = popup(mole);  
+
     game = setInterval(() => {    
-
         mole = selectMole(moles);
-        timer = popup(mole);
-        
-    }, 3000) //todo ramp up time between moles
+        timer = popup(mole);     
+    }, timeBetweenMoles);
 
-    //todo countdown game timer
+    //start/unpause countdown game timer
+    console.log(timeToCountdownFrom)
+    startTimer(document.getElementById("timeLeft"), timeToCountdownFrom);
 }
 
 /**
  * Pause the game by clearing the game's interval ID
  */
 function pauseGame() {
+    //if game has ended/is already paused, prevent pausing
+    if(ended || paused) return;
     console.log("Game paused.");
     updateGameStateText(false, "Game paused.");
     clearInterval(game);
+    pauseTimer();
     //to avoid having to un/re-register game area click listener
-    //use global paused variable to prevent score updates
+    //use global paused variable to prevent firing and score updates
     paused = true;
     //toggle buttons
-    document.getElementById("pauseBtn").classList.add("disabled");
-    document.getElementById("pauseBtn").setAttribute("aria-pressed", true);
-    document.getElementById("startBtn").classList.remove("disabled");
-    document.getElementById("startBtn").setAttribute("aria-pressed", false);
+    updateButton("pauseBtn", false);
+    updateButton("startBtn", true);
 }
 
 /**
  * Reset the game timer and clear the current score and game state text
  */
 function resetGame() {
+    ended = false;
     pauseGame();
+    //override from pauseGame()
+    updateButton("startBtn", true);
+
     updateGameStateText(true);
     updateScore(0);
-    gameTimer = totalGameTime;
+    timeToCountdownFrom = totalGameTime;
+    setTimerValue(document.getElementById("timeLeft"), timeToCountdownFrom)
+}
+
+/**
+ * End the game by pausing it, disabling the start and pause buttons,
+ * and updating the game state text
+ */
+function endGame() {
+    clearInterval(game);
+    clearInterval(gameTimer);
+    paused = true;
+    ended = true;
+    updateGameStateText(false, "Game over! Final score: " + score);
+    updateButton("pauseBtn", false);
+    updateButton("startBtn", false);
+}
+
+/**
+ * Start a countdown timer, and store a reference to it in the global gameTimer
+ * @param {Element} gameTimerText html element to display timer inside
+ * @param {number} duration how long to countdown for in milliseconds
+ */
+function startTimer(gameTimerText, duration) {
+    let start, diff;
+    start = Date.now();
+
+    function timer() {
+        //get the number of seconds that have elapsed since startTimer() was called
+        diff = duration - (parseInt(Date.now() - start));
+        //store current time elapsed to facilitate restarting of timer
+        timeToCountdownFrom = diff;
+        
+        //set in dom
+        setTimerValue(gameTimerText, diff);
+        
+        //get difference in seconds
+        diff = diff / 1000;
+        //if timer reaches zero
+        if(diff <= 0) endGame();
+    };
+    //exec timer() once first, to start the timer without waiting a whole second
+    timer();
+    gameTimer = setInterval(timer, 1000);
+}
+
+/**
+ * Pause the game's countdown timer
+ */
+function pauseTimer() {
+    clearInterval(gameTimer);
+}
+
+/**
+ * Set the timer's value
+ * @param {Element} gameTimerText 
+ * @param {number} time in milliseoncds
+ */
+function setTimerValue(gameTimerText, time) {
+    //convert to seconds
+    time = time / 1000
+
+    //truncate
+    let minutes = parseInt(time / 60);
+    let seconds = parseInt(time % 60);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    gameTimerText.innerHTML = minutes + ":" + seconds;
 }
 
 /**
@@ -111,7 +198,6 @@ function resetGame() {
 function clickHandler(e) {
     //prevent score updates
     if(paused) return;
-    console.log(e.target);
     if(
         e.target.dataset 
         && e.target.dataset.moleState 
@@ -129,7 +215,7 @@ function clickHandler(e) {
 function selectMole(moles) {
     let max = moles.length;
     let num = Math.floor(Math.random() * max);
-    console.log(num);
+    console.log("Mole chosen: " + num);
     return moles[num];
 }
 
@@ -158,7 +244,9 @@ function popdown(mole) {
 }
 
 /**
- * Handle game updates for an unsuccessful mole whack
+ * Handle game updates for a successful mole whack
+ * @param {Element} mole HTML element to be moved
+ * @param {number} timer timer ID to be cleared
  */
 function moleClickHit(mole, timer) {
     console.log("Hit!");
@@ -206,4 +294,17 @@ function updateGameStateText(hide, text) {
     }
     if(text)
         gameState.innerHTML = text;
+}
+
+/**
+ * Visually enable/disable the button with id buttonName
+ * By adding/removing 'disabled' css class 
+ * @param {string} buttonName id of the button to set state of
+ * @param {boolean} state true to enable button, false to disable button
+ */
+function updateButton(buttonName, state) {
+    let button = document.getElementById(buttonName);
+    state ?  
+        button.classList.remove("disabled") : button.classList.add("disabled");
+    button.setAttribute("aria-pressed", !state);
 }
